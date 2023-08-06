8da8e78297377dddb86c5e2344003153c06a4f6c
MiguelLarios <mrlarios5@gmail.com>
2023-08-06 17:19:54 -0400

    update

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log('connected')
+console.log('main.js connected');
+
+const searchTermsInputs = document.body.querySelector("#search-terms");
+
+const getMealCatagories = async () =>{
+const mealCatagoriesApiURL ="https://www.themealdb.com/api/json/v1/1/categories.php"
+
+try {
+    const response = await fetch(mealCatagoriesApiURL)
+    const data = response.json()
+    const catagories = data.catagories 
+    console.log('catagories: ' , catagories);
+}   catch (error) {
+    console.log(error)
+    alert('Something went wrong, try again later');
+}
+
+}
+
+const handleFormInputFocus = async () => {
+    console.log('focus occurred');
+
+await    getMealCatagories();
+};
+
+document.body.addEventListener('focus', handleFormInputFocus);
