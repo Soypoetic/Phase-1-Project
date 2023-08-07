4629ac66b76ee433d210452f2cbe387b772e845b
MiguelLarios <mrlarios5@gmail.com>
2023-08-07 06:36:00 -0400

    update2

--- main.js
+++ main.js
@@ -2,25 +2,76 @@ console.log('main.js connected');
 
 const searchTermsInputs = document.body.querySelector("#search-terms");
 
+
+//fetch meal categories data from TheMealBD API
 const getMealCatagories = async () =>{
-const mealCatagoriesApiURL ="https://www.themealdb.com/api/json/v1/1/categories.php"
+    const mealCatagoriesApiURL ="www.themealdb.com/api/json/v1/1/categories.php"
 
 try {
     const response = await fetch(mealCatagoriesApiURL)
-    const data = response.json()
+    const data = await response.json()
     const catagories = data.catagories 
     console.log('catagories: ' , catagories);
 }   catch (error) {
     console.log(error)
     alert('Something went wrong, try again later');
-}
+    }
 
-}
+};
 
+// render meal catagories data to dom
+const renderMealCatagories = (mealCatagoriesObj) => {
+    console.log('renderMealCatagories');
+    console.table('focus ocurred');
+
+//find and select a UL with meal catagory dom element to append my data into
+const mealCatagoriesList =document.body.querySelector('#meal-categories-list')
+
+//for each element in our meal catagories array 
+mealCategoriesArray.forEach(mealCategory =>{
+
+    // create wrapping element maybe <li> class card 
+    const mealCategoryListItem = document.createElement('li')
+    mealCatagoriesListItem.className = 'meal-category-card'
+    //appendChild the li.card to selected DOM Element
+    mealCatagoriesList.appendChild(mealCategoryListItem)
+
+        //display the catagory name, image, and description
+        //image
+        // create an img element
+        const mealCategoryImg = document.createElement('img')
+            //set img src to catagory thumbail url
+            mealCategoryImg.scr = mealcategory?.strCategoryThumb
+            //give it a mobile friendly max width of like 300px
+            mealCategoryImg.width = 300
+            //appendChild the image to our card
+            mealCategoryListItem.appendChild(mealCategoryImg);
+
+        //name
+        //create an h4 element
+        const mealCategoryName = document.createElement('h4')
+            // set h4 element textContent to be meal catagory name
+            mealCategoryName.textContent = mealCategory?.strCategory;
+            //appendChild the h4 to our card
+            mealCategoryListItem.appendChild(mealcategoryName);
+
+        //description
+        //create an p element
+        const mealCategoryDescription = document.createElement('p')
+            // set p element textContent to be meal catagory description
+            mealCategoryDescription.textContent = mealCategory?.strCategoryDescription;
+            //appendChild the p to our card
+            mealCategoryListItem.appendChild(mealCategoryDescription);
+        });
+};
+
+// funtion to call when form inputs is given focus
 const handleFormInputFocus = async () => {
     console.log('focus occurred');
 
-await    getMealCatagories();
+    const mealCatagoriesObj =await getMealCatagories();
+    renderMealCatagories('mealCategoriesObj');
 };
 
+//add event listener to search term input
 document.body.addEventListener('focus', handleFormInputFocus);
